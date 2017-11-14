772c187bb47ee7d0a4c356052b6d785f1042eb12
rochars <rocha.rafaelsilva@gmail.com>
2017-11-14 01:04:50 -0200

    WaveFile extends WaveFileHeader

--- dist/wavefile.js
+++ dist/wavefile.js
@@ -662,13 +662,12 @@ module.exports.read64Bit = read64Bit;
  */
 
 const byteData = __webpack_require__(8);
+const wavefileheader = __webpack_require__(13);
 
 /**
  * A wave file.
- * Objects can be created by passing a Uint8Array
- * representing a wave file.
  */
-class WaveFile {
+class WaveFile extends wavefileheader.WaveFileHeader {
 
     /**
      * @param {Uint8Array} bytes The file bytes.
@@ -678,72 +677,15 @@ class WaveFile {
      *      if no "bext" chunk is found.
      */
     constructor(bytes, enforceFact=false, enforceBext=false) {
+        super();
         /** @type {boolean} */
         this.enforceFact = enforceFact;
         /** @type {boolean} */
-        this.enforceBext = enforceBext;
+        this.enforceBext = enforceBext;        
         /**
-         * "RIFF"
-         * @type {string}
-         */
-        this.chunkId = "";
-        /** @type {number} */
-        this.chunkSize = 0;
-        /**
-         * "WAVE"
-         * @type {string}
-         */
-        this.subChunk1Id = "";
-        /**
-         * "fmt "
-         * @type {string}
-         */
-        this.format = "";
-        /** @type {number} */
-        this.subChunk1Size = 0;
-        /** @type {number} */
-        this.audioFormat = 0;
-        /** @type {number} */
-        this.numChannels = 0;
-        /** @type {number} */
-        this.sampleRate = 0;
-        /** @type {number} */
-        this.byteRate = 0;
-        /** @type {number} */
-        this.blockAlign = 0;
-        /** @type {number} */
-        this.bitsPerSample = 0;
-        /** @type {string} */
-        this.factChunkId = "";
-        /**
-         * minimum 4
-         * @type {number}
-         */
-        this.factChunkSize = 0; // 
-        /** @type {number} */
-        this.dwSampleLength = 0;
-        /**
-         * "data"
-         * @type {string}
-         */
-        this.subChunk2Id = "";
-        /** @type {number} */
-        this.subChunk2Size = 0;
-        /** @type {!Array<number>} */
-        this.samples_ = [];
-        /** @type {!Array<number>} */
-        this.bytes_ = [];
-        /** @type {string} */
-        this.bitDepth_ = "";
-        /**
-         * "bext"
-         * @type {string}
-         */
-        this.bextChunkId = "";
-        /**
-         * Wave file formats according to the specs.
+         * Header formats.
          * @enum {number}
-         */        
+         */
         this.headerFormats_ = {
             "4": 17,
             "8": 1,
@@ -1955,6 +1897,87 @@ module.exports.packCrumbs = packCrumbs;
 module.exports.unpackCrumbs = unpackCrumbs;
 module.exports.packNibbles = packNibbles;
 module.exports.unpackNibbles = unpackNibbles;
+
+
+/***/ }),
+/* 13 */
+/***/ (function(module, exports) {
+
+/*
+ * WaveFileHeader
+ * Copyright (c) 2017 Rafael da Silva Rocha. MIT License.
+ * https://github.com/rochars/wavefile
+ *
+ */
+
+/**
+ * Wave file headers.
+ */
+class WaveFileHeader {
+
+    constructor() {
+        /**
+         * "RIFF"
+         * @type {string}
+         */
+        this.chunkId = "";
+        /** @type {number} */
+        this.chunkSize = 0;
+        /**
+         * "WAVE"
+         * @type {string}
+         */
+        this.subChunk1Id = "";
+        /**
+         * "fmt "
+         * @type {string}
+         */
+        this.format = "";
+        /** @type {number} */
+        this.subChunk1Size = 0;
+        /** @type {number} */
+        this.audioFormat = 0;
+        /** @type {number} */
+        this.numChannels = 0;
+        /** @type {number} */
+        this.sampleRate = 0;
+        /** @type {number} */
+        this.byteRate = 0;
+        /** @type {number} */
+        this.blockAlign = 0;
+        /** @type {number} */
+        this.bitsPerSample = 0;
+        /** @type {string} */
+        this.factChunkId = "";
+        /**
+         * minimum 4
+         * @type {number}
+         */
+        this.factChunkSize = 0;
+        /** @type {number} */
+        this.dwSampleLength = 0;
+        /**
+         * "data"
+         * @type {string}
+         */
+        this.subChunk2Id = "";
+        /** @type {number} */
+        this.subChunk2Size = 0;
+        /** @type {!Array<number>} */
+        this.samples_ = [];
+        /** @type {!Array<number>} */
+        this.bytes_ = [];
+        /** @type {string} */
+        this.bitDepth_ = "";
+        /**
+         * "bext"
+         * @type {string}
+         */
+        this.bextChunkId = "";
+    }
+}
+
+module.exports.WaveFileHeader = WaveFileHeader;
 
 
 /***/ })
---
+++ src/wavefileheader.js
@@ -0,0 +1,75 @@
+/*
+ * WaveFileHeader
+ * Copyright (c) 2017 Rafael da Silva Rocha. MIT License.
+ * https://github.com/rochars/wavefile
+ *
+ */
+
+/**
+ * Wave file headers.
+ */
+class WaveFileHeader {
+
+    constructor() {
+        /**
+         * "RIFF"
+         * @type {string}
+         */
+        this.chunkId = "";
+        /** @type {number} */
+        this.chunkSize = 0;
+        /**
+         * "WAVE"
+         * @type {string}
+         */
+        this.subChunk1Id = "";
+        /**
+         * "fmt "
+         * @type {string}
+         */
+        this.format = "";
+        /** @type {number} */
+        this.subChunk1Size = 0;
+        /** @type {number} */
+        this.audioFormat = 0;
+        /** @type {number} */
+        this.numChannels = 0;
+        /** @type {number} */
+        this.sampleRate = 0;
+        /** @type {number} */
+        this.byteRate = 0;
+        /** @type {number} */
+        this.blockAlign = 0;
+        /** @type {number} */
+        this.bitsPerSample = 0;
+        /** @type {string} */
+        this.factChunkId = "";
+        /**
+         * minimum 4
+         * @type {number}
+         */
+        this.factChunkSize = 0;
+        /** @type {number} */
+        this.dwSampleLength = 0;
+        /**
+         * "data"
+         * @type {string}
+         */
+        this.subChunk2Id = "";
+        /** @type {number} */
+        this.subChunk2Size = 0;
+        /** @type {!Array<number>} */
+        this.samples_ = [];
+        /** @type {!Array<number>} */
+        this.bytes_ = [];
+        /** @type {string} */
+        this.bitDepth_ = "";
+        /**
+         * "bext"
+         * @type {string}
+         */
+        this.bextChunkId = "";
+    }
+}
+
+module.exports.WaveFileHeader = WaveFileHeader;
