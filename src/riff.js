7a10038164edb65183db4f35a31d3bddb9a5e161
rochars <rocha.rafaelsilva@gmail.com>
2017-11-28 16:15:44 -0200

    riff.getSubChunks()

--- src/riff.js
+++ src/riff.js
@@ -16,23 +16,33 @@ const byteData = require("byte-data");
  * @return {Object}
  */
 function getChunks(buffer, bigEndian) {
-    
-    // RIFF container
-    let chunkId = byteData.fromBytes(
-            buffer.slice(0, 4), 8, {"char": true}
-        );
-    let chunkSize = byteData.fromBytes(
-            buffer.slice(4, 8), 32, {'be': bigEndian}
-        )[0];
-    let format = byteData.fromBytes(
-            buffer.slice(8, 12), 8, {"char": true}
-        );
+    return {
+        "chunkId": byteData.fromBytes(
+                buffer.slice(0, 4), 8, {"char": true}
+            ),
+        "chunkSize": byteData.fromBytes(
+                buffer.slice(4, 8), 32, {'be': bigEndian}
+            )[0],
+        "format": byteData.fromBytes(
+                buffer.slice(8, 12), 8, {"char": true}
+            ),
+        "subChunks": getSubChunks(buffer, bigEndian)
+    }
+}
 
+/**
+ * List the sub chunks of a RIFF file.
+ * @param {Uint8Array|!Array<number>} buffer the RIFF file bytes.
+ * @param {boolean} bigEndian true if its RIFX.
+ * @return {Object}
+ */
+function getSubChunks(buffer, bigEndian) {
     let chunks = [];
     let len = buffer.length;
     let i = 12;
+    let subChunkSize;
     while(i < len) {
-        let subChunkSize = byteData.fromBytes(
+        subChunkSize = byteData.fromBytes(
             buffer.slice(i + 4, i + 8), 32, {'be': bigEndian})[0];
         chunks.push({
                 "subChunkId": byteData.fromBytes(buffer.slice(i, i + 4), 8, {"char": true}),
@@ -41,12 +51,7 @@ function getChunks(buffer, bigEndian) {
             });
         i = i + 8 + subChunkSize;
     }
-    return {
-        "chunkId": chunkId,
-        "chunkSize": chunkSize,
-        "format": format,
-        "subChunks": chunks
-    }
+    return chunks;
 }
 
 module.exports.getChunks = getChunks;
