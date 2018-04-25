6cf6ca598c08f1c77029c2ab37d60f508879ec87
Rafael Rocha <rocha.rafaelsilva@gmail.com>
2018-04-25 02:32:59 -0300

    running the tests against original and minified versions

---
+++ test/loader.js
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2018 Rafael da Silva Rocha.
+ * https://github.com/rochars/wavefile
+ *
+ */
+
+let wavefile = require('../index.js');
+
+if (process.argv[3] == '--dist') {
+    require('browser-env')();let assert = require('assert');
+    require('../dist/wavefile-min.js');
+    wavefile = window.wavefile;
+}
+
+module.exports = wavefile;
