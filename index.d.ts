35eb7d7793896f225e14c66caa78e37aaf2ae5a9
Rafael Rocha <rocha.rafaelsilva@gmail.com>
2018-06-29 03:09:57 -0300

    Update docstring to version 8.0

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for wavefile 7.0
+// Type definitions for wavefile 8.0
 // Project: https://github.com/rochars/wavefile
 // Definitions by: Rafael S. Rocha <https://github.com/rochars>
 // Definitions: https://github.com/rochars/wavefile
