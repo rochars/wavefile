2cb959d04e0f6d1ed01c9ce570b7648cdce06b13
Rafael Rocha <rocha.rafaelsilva@gmail.com>
2019-08-02 16:35:47 -0300

    Add test for WaveFileParser class

---
+++ test/wavefile-parser/write-buffer.js
@@ -0,0 +1,60 @@
+/**
+ * WaveFile: https://github.com/rochars/wavefile
+ * Copyright (c) 2017-2018 Rafael da Silva Rocha. MIT License.
+ *
+ * Tests for the WaveFileParser class readBuffer method.
+ * 
+ */
+
+const assert = require('assert');
+const fs = require("fs");
+const WaveFileParser = require("../../lib/wavefile-parser.js").default;
+const path = "./test/files/";
+
+describe('read 16bit file from disk and write to new file', function() {
+    
+    let wav = new WaveFileParser();
+    wav.readBuffer(fs.readFileSync(path + "16-bit-8kHz-noBext-mono.wav"));
+    let wav2 = new WaveFileParser();
+    wav2.readBuffer(wav.writeBuffer());
+
+    it("chunkId should be 'RIFF'", function() {
+        assert.equal(wav2.container, "RIFF");
+    });
+    it("fmtChunkId should be 'fmt '", function() {
+        assert.equal(wav2.fmt.chunkId, "fmt ");
+    });
+    it("format should be 'WAVE'", function() {
+        assert.equal(wav2.format, "WAVE");
+    });
+    it("fmtChunkSize should be 16", function() {
+        assert.equal(wav2.fmt.chunkSize, 16);
+    });
+    it("audioFormat should be 1 (PCM)", function() {
+        assert.equal(wav2.fmt.audioFormat, 1);
+    });
+    it("numChannels should be 1", function() {
+        assert.equal(wav2.fmt.numChannels, 1);
+    });
+    it("sampleRate should be 8000", function() {
+        assert.equal(wav2.fmt.sampleRate, 8000);
+    });
+    it("byteRate should be 16000", function() {
+        assert.equal(wav2.fmt.byteRate, 16000);
+    });
+    it("blockAlign should be 2", function() {
+        assert.equal(wav2.fmt.blockAlign, 2);
+    });
+    it("bitsPerSample should be 16", function() {
+        assert.equal(wav2.fmt.bitsPerSample, 16);
+    });
+    it("dataChunkId should be 'data'", function() {
+        assert.equal(wav2.data.chunkId, 'data');
+    });
+    it("dataChunkSize should be > 0", function() {
+        assert.ok(wav2.data.chunkSize > 0);
+    });
+    it("samples.length should be > 0", function() {
+        assert.ok(wav2.data.samples.length > 0);
+    });
+});
