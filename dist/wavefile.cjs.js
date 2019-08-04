3420dade2b53ff08db3e0e712cb7771cbb79ffe1
Rafael Rocha <rocha.rafaelsilva@gmail.com>
2019-08-04 03:42:20 -0300

    validateSampleRate as a function

--- dist/wavefile.cjs.js
+++ dist/wavefile.cjs.js
@@ -2234,6 +2234,52 @@ function writeString(str, maxSize, fixedSize=true) {
  */
 
 /**
+ * @fileoverview The validateSampleRate function.
+ * @see https://github.com/rochars/wavefile
+ */
+
+/**
+ * Validate the sample rate value of a wav file according to the number of
+ * channels and the bit depth of the audio.
+ * @param {number} channels The number of channels in the file.
+ * @param {number} bits The number of bits per sample.
+ * @param {number} sampleRate The sample rate to be validated.
+ * @return {boolean} True is the sample rate is valid, false otherwise.
+ */
+function validateSampleRate(channels, bits, sampleRate) {
+  /** @type {number} */
+  let byteRate = channels * (bits / 8) * sampleRate;
+  if (sampleRate < 1 || byteRate > 4294967295) {
+    return false;
+  }
+  return true;
+}
+
+/*
+ * Copyright (c) 2017-2019 Rafael da Silva Rocha.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ */
+
+/**
  * A class to read and write wav files.
  */
 class WaveFileParser extends RIFFFile {
@@ -2531,7 +2577,10 @@ class WaveFileParser extends RIFFFile {
   validateWavHeader() {
     this.validateBitDepth_();
     this.validateNumChannels_();
-    this.validateSampleRate_();
+    if (!validateSampleRate(
+        this.fmt.numChannels, this.fmt.bitsPerSample, this.fmt.sampleRate)) {
+      throw new Error('Invalid sample rate.');
+    }
   }
 
   /**
@@ -3269,22 +3318,6 @@ class WaveFileParser extends RIFFFile {
     let blockAlign = this.fmt.numChannels * this.fmt.bitsPerSample / 8;
     if (this.fmt.numChannels < 1 || blockAlign > 65535) {
       throw new Error('Invalid number of channels.');
-    }
-    return true;
-  }
-
-  /**
-   * Validate the sample rate value.
-   * @return {boolean} True is the sample rate is valid.
-   * @throws {Error} If the sample rate is invalid.
-   * @private
-   */
-  validateSampleRate_() {
-    /** @type {number} */
-    let byteRate = this.fmt.numChannels *
-      (this.fmt.bitsPerSample / 8) * this.fmt.sampleRate;
-    if (this.fmt.sampleRate < 1 || byteRate > 4294967295) {
-      throw new Error('Invalid sample rate.');
     }
     return true;
   }
