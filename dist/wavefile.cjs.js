f9561417d175a057cbbfd45b93787716c48a29d5
Rafael Rocha <rocha.rafaelsilva@gmail.com>
2019-08-04 03:52:17 -0300

    validateNumChannels as a function

--- dist/wavefile.cjs.js
+++ dist/wavefile.cjs.js
@@ -2234,6 +2234,51 @@ function writeString(str, maxSize, fixedSize=true) {
  */
 
 /**
+ * @fileoverview The validateNumChannels function.
+ * @see https://github.com/rochars/wavefile
+ */
+
+/**
+ * Validate the number of channels in a wav file according to the
+ * bit depth of the audio.
+ * @param {number} channels The number of channels in the file.
+ * @param {number} bits The number of bits per sample.
+ * @return {boolean} True is the number of channels is valid.
+ */
+function validateNumChannels(channels, bits) {
+  /** @type {number} */
+  let blockAlign = channels * bits / 8;
+  if (channels < 1 || blockAlign > 65535) {
+    return false;
+  }
+  return true;
+}
+
+/*
+ * Copyright (c) 2017-2019 Rafael da Silva Rocha.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ */
+
+/**
  * @fileoverview The validateSampleRate function.
  * @see https://github.com/rochars/wavefile
  */
@@ -2576,7 +2621,9 @@ class WaveFileParser extends RIFFFile {
    */
   validateWavHeader() {
     this.validateBitDepth_();
-    this.validateNumChannels_();
+    if (!validateNumChannels(this.fmt.numChannels, this.fmt.bitsPerSample)) {
+      throw new Error('Invalid number of channels.');
+    }
     if (!validateSampleRate(
         this.fmt.numChannels, this.fmt.bitsPerSample, this.fmt.sampleRate)) {
       throw new Error('Invalid sample rate.');
@@ -3303,21 +3350,6 @@ class WaveFileParser extends RIFFFile {
         return true;
       }
       throw new Error('Invalid bit depth.');
-    }
-    return true;
-  }
-
-  /**
-   * Validate the number of channels.
-   * @return {boolean} True is the number of channels is valid.
-   * @throws {Error} If the number of channels is invalid.
-   * @private
-   */
-  validateNumChannels_() {
-    /** @type {number} */
-    let blockAlign = this.fmt.numChannels * this.fmt.bitsPerSample / 8;
-    if (this.fmt.numChannels < 1 || blockAlign > 65535) {
-      throw new Error('Invalid number of channels.');
     }
     return true;
   }
