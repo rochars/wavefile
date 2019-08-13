133a0d1cb2258603d7769f0773a8f516bb3ca4b3
Rafael Rocha <rocha.rafaelsilva@gmail.com>
2019-08-12 23:24:04 -0300

    Move WAV_AUDIO_FORMATS to WaveFileParser

--- dist/wavefile.cjs.js
+++ dist/wavefile.cjs.js
@@ -2165,97 +2165,6 @@ class RIFFFile {
  */
 
 /**
- * @fileoverview The validateNumChannels function.
- * @see https://github.com/rochars/wavefile
- */
-
-/**
- * Validate the number of channels in a wav file according to the
- * bit depth of the audio.
- * @param {number} channels The number of channels in the file.
- * @param {number} bits The number of bits per sample.
- * @return {boolean} True is the number of channels is valid.
- */
-function validateNumChannels(channels, bits) {
-  /** @type {number} */
-  let blockAlign = channels * bits / 8;
-  if (channels < 1 || blockAlign > 65535) {
-    return false;
-  }
-  return true;
-}
-
-/*
- * Copyright (c) 2017-2019 Rafael da Silva Rocha.
- *
- * Permission is hereby granted, free of charge, to any person obtaining
- * a copy of this software and associated documentation files (the
- * "Software"), to deal in the Software without restriction, including
- * without limitation the rights to use, copy, modify, merge, publish,
- * distribute, sublicense, and/or sell copies of the Software, and to
- * permit persons to whom the Software is furnished to do so, subject to
- * the following conditions:
- *
- * The above copyright notice and this permission notice shall be
- * included in all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
- * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
- * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
- * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
- * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
- * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- *
- */
-
-/**
- * @fileoverview The validateSampleRate function.
- * @see https://github.com/rochars/wavefile
- */
-
-/**
- * Validate the sample rate value of a wav file according to the number of
- * channels and the bit depth of the audio.
- * @param {number} channels The number of channels in the file.
- * @param {number} bits The number of bits per sample.
- * @param {number} sampleRate The sample rate to be validated.
- * @return {boolean} True is the sample rate is valid, false otherwise.
- */
-function validateSampleRate(channels, bits, sampleRate) {
-  /** @type {number} */
-  let byteRate = channels * (bits / 8) * sampleRate;
-  if (sampleRate < 1 || byteRate > 4294967295) {
-    return false;
-  }
-  return true;
-}
-
-/*
- * Copyright (c) 2017-2019 Rafael da Silva Rocha.
- *
- * Permission is hereby granted, free of charge, to any person obtaining
- * a copy of this software and associated documentation files (the
- * "Software"), to deal in the Software without restriction, including
- * without limitation the rights to use, copy, modify, merge, publish,
- * distribute, sublicense, and/or sell copies of the Software, and to
- * permit persons to whom the Software is furnished to do so, subject to
- * the following conditions:
- *
- * The above copyright notice and this permission notice shall be
- * included in all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
- * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
- * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
- * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
- * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
- * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- *
- */
-
-/**
  * A class to read wav files.
  */
 class WaveFileReader extends RIFFFile {
@@ -2271,24 +2180,6 @@ class WaveFileReader extends RIFFFile {
     super();
     // Include 'RF64' as a supported container format
     this.supported_containers.push('RF64');
-    /**
-     * Audio formats.
-     * Formats not listed here should be set to 65534,
-     * the code for WAVE_FORMAT_EXTENSIBLE
-     * @enum {number}
-     * @protected
-     */
-    this.WAV_AUDIO_FORMATS = {
-      '4': 17,
-      '8': 1,
-      '8a': 6,
-      '8m': 7,
-      '16': 1,
-      '24': 1,
-      '32': 1,
-      '32f': 3,
-      '64': 3
-    };
     /**
      * The data of the 'fmt' chunk.
      * @type {!Object<string, *>}
@@ -3121,10 +3012,123 @@ function writeString(str, maxSize, fixedSize=true) {
  */
 
 /**
+ * @fileoverview The validateNumChannels function.
+ * @see https://github.com/rochars/wavefile
+ */
+
+/**
+ * Validate the number of channels in a wav file according to the
+ * bit depth of the audio.
+ * @param {number} channels The number of channels in the file.
+ * @param {number} bits The number of bits per sample.
+ * @return {boolean} True is the number of channels is valid.
+ */
+function validateNumChannels(channels, bits) {
+  /** @type {number} */
+  let blockAlign = channels * bits / 8;
+  if (channels < 1 || blockAlign > 65535) {
+    return false;
+  }
+  return true;
+}
+
+/*
+ * Copyright (c) 2017-2019 Rafael da Silva Rocha.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ */
+
+/**
+ * @fileoverview The validateSampleRate function.
+ * @see https://github.com/rochars/wavefile
+ */
+
+/**
+ * Validate the sample rate value of a wav file according to the number of
+ * channels and the bit depth of the audio.
+ * @param {number} channels The number of channels in the file.
+ * @param {number} bits The number of bits per sample.
+ * @param {number} sampleRate The sample rate to be validated.
+ * @return {boolean} True is the sample rate is valid, false otherwise.
+ */
+function validateSampleRate(channels, bits, sampleRate) {
+  /** @type {number} */
+  let byteRate = channels * (bits / 8) * sampleRate;
+  if (sampleRate < 1 || byteRate > 4294967295) {
+    return false;
+  }
+  return true;
+}
+
+/*
+ * Copyright (c) 2017-2019 Rafael da Silva Rocha.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ */
+
+/**
  * A class to read and write wav files.
  * @extends WaveFileReader
  */
 class WaveFileParser extends WaveFileReader {
+
+  constructor(wavBuffer=null) {
+    super(wavBuffer);
+    /**
+     * Audio formats.
+     * Formats not listed here should be set to 65534,
+     * the code for WAVE_FORMAT_EXTENSIBLE
+     * @enum {number}
+     * @protected
+     */
+    this.WAV_AUDIO_FORMATS = {
+      '4': 17,
+      '8': 1,
+      '8a': 6,
+      '8m': 7,
+      '16': 1,
+      '24': 1,
+      '32': 1,
+      '32f': 3,
+      '64': 3
+    };
+  }
 
   /**
    * Return a byte buffer representig the WaveFileParser object as a .wav file.
