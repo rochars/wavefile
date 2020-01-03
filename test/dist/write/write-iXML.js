514db624e0823f190a68cd1004891d27eadbbd48
Rafael Rocha <rocha.rafaelsilva@gmail.com>
2020-01-03 16:03:38 -0300

    Add tests for iXML value change

---
+++ test/dist/write/write-iXML.js
@@ -0,0 +1,49 @@
+/**
+ * WaveFile: https://github.com/rochars/wavefile
+ * Copyright (c) 2017-2020 Rafael da Silva Rocha. MIT License.
+ *
+ * Write files with iXML chunk.
+ * 
+ */
+
+const assert = require("assert");
+const fs = require("fs");
+const WaveFile = require("../../../test/loader.js");
+const path = "./test/files/";
+
+describe("Read files with iXML chunk", function() {
+    let wav = new WaveFile(
+        fs.readFileSync(path + "24bit-48kHz-1c-mixpre6-hiser_interview.WAV"));
+
+    it("iXML bytes should be a even number", function() {
+        assert.equal(wav.get_PMXBytes_().length % 2, false);
+    });
+
+    it("iXML bytes should be a even number on written file", function() {
+    	let wav2 = new WaveFile(wav.toBuffer());
+        assert.equal(wav2.get_PMXBytes_().length % 2, false);
+    });
+});
+
+
+describe("Change the value of iXML chunk", function() {
+	let wav = new WaveFile(
+        fs.readFileSync(path + "24bit-48kHz-1c-mixpre6-hiser_interview.WAV"));
+    wav.iXML.value = '<pmx/>';
+    let wav2 = new WaveFile(wav.toBuffer());
+    wav2.toBuffer();
+
+    it("should find the 'iXML' chunk", function() {
+        assert.equal(wav2.iXML.chunkId, "iXML");
+    });
+    it("'iXML' chunkSize should be 6", function() {
+        assert.equal(wav2.iXML.chunkSize, 6);
+    });
+    it("'iXML' value should be a string with length 6", function() {
+        assert.equal(wav2.iXML.value.length, 6);
+    });
+    it("'iXML' value in new file should be equal to the one in the original file", function() {
+        assert.equal(wav2.iXML.value, '<pmx/>');
+    });
+
+});
